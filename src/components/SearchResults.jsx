cff732022dd50eb7e9be565ed6a8c7a9450a341a
unknown <minsoojo5@gmail.com>
2023-12-05 13:09:57 -0500

    making more changes to layouts

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -10,16 +10,30 @@ export default function SearchResults({
   let angle = angleCalculation(knifeStyle, metalType, singleBevel);
   return stones[0] != 0 ? (
     <div>
-      <h3>Use these stones in order</h3>
       <h3>
-        With an angle of {angle - 1} - {angle + 1} degrees
+        Stones to use with an angle of {angle - 1} - {angle + 1} degrees
       </h3>
-      <h3>{singleBevel === "Yes" ? "On one side only" : "On both sides"}</h3>
+      <h3>
+        {singleBevel !== "" && singleBevel === "Yes"
+          ? "On one side only"
+          : singleBevel === "No"
+          ? "On both sides"
+          : null}
+      </h3>
       <ul>
         {stones.map((stone) => (
           <li key={stones.indexOf(stone)}>Grit Level: {stone}</li> // Place holder key for the eslint error.
         ))}
       </ul>
+      <h3>
+        {currentSharpness !== "" && currentSharpness === "Very Dull"
+          ? "Time to take off some metal!"
+          : currentSharpness === "Dull"
+          ? "You're gonna need some elbow grease!"
+          : currentSharpness === "Almost Sharp"
+          ? "Just a little touch up!"
+          : null}
+      </h3>
     </div>
   ) : null;
 }

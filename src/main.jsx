74b1c12afeac208eee36b73afd53b3d46e5a1ff0
unknown <minsoojo5@gmail.com>
2023-12-05 14:34:33 -0500

    Little bit of meta data stuff

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <img
+      src="https://static.thenounproject.com/png/892542-200.png"
+      width="5%"
+    />
   </React.StrictMode>
 );

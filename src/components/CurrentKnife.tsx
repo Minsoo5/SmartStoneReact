0f440692e2690d1b45fb77d0a74d47e6b3ffd162
unknown <minsoojo5@gmail.com>
2023-12-06 14:12:23 -0500

    clean up

--- src/components/CurrentKnife.tsx
+++ src/components/CurrentKnife.tsx
@@ -20,38 +20,3 @@ const CurrentKnife = (props: knifeObject) => {
 };
 
 export default CurrentKnife;
-
-// let sharpness: string = "";
-// let style: string = "";
-// let metal: string = "";
-// let bevel: string = "";
-
-// props.currentSharpness !== "" && props.currentSharpness == "Very Dull"
-//   ? (sharpness = "A Very Dull")
-//   : props.currentSharpness == "Dull"
-//   ? (sharpness = "A Dull")
-//   : props.currentSharpness == "Almost Sharp"
-//   ? (sharpness = "An Almost Sharp")
-//   : null;
-
-// props.knifeStyle !== "" && props.knifeStyle === "Chef Knife"
-//   ? (style = "Chef Knife")
-//   : props.knifeStyle === "Butcher Knife"
-//   ? (style = "Butcher Knife")
-//   : props.knifeStyle === "Slicing Knife"
-//   ? (style = "Slicing Knife")
-//   : null;
-
-// props.metalType !== "" && props.metalType === "High Carbon"
-//   ? (metal = "High Carbon ")
-//   : props.metalType === "Stainless Steel"
-//   ? (metal = "Stainless Steel ")
-//   : props.metalType === "Damascus"
-//   ? (metal = "Damascus ")
-//   : null;
-
-// props.singleBevel !== "" && props.singleBevel === "Yes"
-//   ? (bevel = "Single Edge")
-//   : props.singleBevel === "No"
-//   ? (bevel = "Double Edge")
-//   : null;

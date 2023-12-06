d5d5231cf4a92533e01b776a10480a803d1da8b5
unknown <minsoojo5@gmail.com>
2023-12-06 11:20:37 -0500

    refactor in Current Knife Display Logic

--- src/components/CurrentKnife.tsx
+++ src/components/CurrentKnife.tsx
@@ -1,36 +1,50 @@
 import { knifeObject } from "./objectTypes";
 
 const CurrentKnife = (props: knifeObject) => {
-  return (
+  let sharpness: string = "";
+  let style: string = "";
+  let metal: string = "";
+  let bevel: string = "";
+
+  props.currentSharpness !== "" && props.currentSharpness == "Very Dull"
+    ? (sharpness = "A Very Dull")
+    : props.currentSharpness == "Dull"
+    ? (sharpness = "A Dull")
+    : props.currentSharpness == "Almost Sharp"
+    ? (sharpness = "An Almost Sharp")
+    : null;
+
+  props.knifeStyle !== "" && props.knifeStyle === "Chef Knife"
+    ? (style = "Chef Knife")
+    : props.knifeStyle === "Butcher Knife"
+    ? (style = "Butcher Knife")
+    : props.knifeStyle === "Slicing Knife"
+    ? (style = "Slicing Knife")
+    : null;
+
+  props.metalType !== "" && props.metalType === "High Carbon"
+    ? (metal = "High Carbon ")
+    : props.metalType === "Stainless Steel"
+    ? (metal = "Stainless Steel ")
+    : props.metalType === "Damascus"
+    ? (metal = "Damascus ")
+    : null;
+
+  props.singleBevel !== "" && props.singleBevel === "Yes"
+    ? (bevel = "Single Edge")
+    : props.singleBevel === "No"
+    ? (bevel = "Double Edge")
+    : null;
+
+  let knife: string = `${sharpness} ${style} made with ${metal} with a ${bevel}`;
+
+  return sharpness !== "" && style !== "" && metal !== "" && bevel !== "" ? (
     <h2>
-      {props.knifeStyle !== "" ||
-      props.metalType !== "" ||
-      props.singleBevel !== "" ||
-      props.currentSharpness !== ""
-        ? "Current Knife: "
-        : null}
+      Current Knife:
       <br></br>
-      {props.singleBevel !== "" && props.singleBevel === "Yes"
-        ? "Single Edged "
-        : props.singleBevel === "No"
-        ? "Double Edged "
-        : null}
-      {props.metalType !== "" && props.metalType === "High Carbon"
-        ? "High Carbon "
-        : props.metalType === "Stainless Steel"
-        ? "Stainless Steel "
-        : props.metalType === "Damascus"
-        ? "Damascus "
-        : null}
-      {props.knifeStyle !== "" && props.knifeStyle === "Chef Knife"
-        ? "Chef Knife"
-        : props.knifeStyle === "Butcher Knife"
-        ? "Butcher Knife"
-        : props.knifeStyle === "Slicing Knife"
-        ? "Slicing Knife"
-        : null}
+      {knife}
     </h2>
-  );
+  ) : null;
 };
 
 export default CurrentKnife;

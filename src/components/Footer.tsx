c58b642833b55c5cb4cb277559ca3eb5026fc080
unknown <minsoojo5@gmail.com>
2023-12-07 14:45:08 -0500

    playing around with css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
       <a href="https://www.korin.com/" target="_blank" rel="noreferrer">
         <img
           src="https://static.thenounproject.com/png/892542-200.png"
+          alt="Knife icon with a link to where you can buy more knives"
           width="4%"
         />
       </a>
@@ -14,6 +15,7 @@ const Footer = () => {
       >
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1024px-YouTube_full-color_icon_%282017%29.svg.png"
+          alt="Youtube icon with a link to a video explaining how to sharpen your knife"
           width="4%"
         />
       </a>
